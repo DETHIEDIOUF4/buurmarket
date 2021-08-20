7dfd3a70c4b31b604bf75dd1cf75d6f61ad918ac
Dethie DIOUF <55641058+DETHIEDIOUF4@users.noreply.github.com>
2021-08-20 10:55:34 +0000

    Update Change

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,17 @@ const Header = () => {
         <>
          <Navbar className="color-nav" variant="dark"  collapseOnSelect expand="lg" sticky="top" >
             <Container>
-                    <Navbar.Brand style={{marginRight:200}} href="/">
+                    <Navbar.Brand style={{marginRight:160}} href="/">
                         <img src={logo} width="" height="30" alt="Buurmarkets"/>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                  <Navbar.Collapse id="basic-navbar-nav">
+                  <Navbar.Collapse id="basic-navbar-nav"  style={{justifyContent:'center',}} >
                 <Nav className="me-auto">
-                <Nav.Link style={{marginRight:45 ,fontWeight:'bold'}} href="/home">Trade</Nav.Link>
-                <Nav.Link style={{marginRight:45 ,fontWeight:'bold'}} href="/service">Produit</Nav.Link>
-             <Nav.Link style={{marginRight:45 , fontWeight:'bold'}} href="/about">CopyPortfolio</Nav.Link>
-             <Nav.Link style={{marginRight:45 ,fontWeight:'bold'}} href="/file">Actualités</Nav.Link>
-             <Nav.Link style={{marginRight:45 , fontWeight:'bold'}} href="/file">Société</Nav.Link>
+                <Nav.Link style={{marginRight:50 ,fontWeight:'bold'}} href="/home">Trade</Nav.Link>
+                <Nav.Link style={{marginRight:50 ,fontWeight:'bold'}} href="/service">Produit</Nav.Link>
+             <Nav.Link style={{marginRight:50 , fontWeight:'bold'}} href="/about">CopyPortfolio</Nav.Link>
+             <Nav.Link style={{marginRight:50 ,fontWeight:'bold'}} href="/file">Actualités</Nav.Link>
+             <Nav.Link style={{ fontWeight:'bold'}} href="/file">Société</Nav.Link>
              </Nav>
                         <Button variant="primary">Se connecter</Button>
                         </Navbar.Collapse>   
